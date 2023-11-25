9068905433aea5f4d3b931bdaf6760da827285b8
zzzt4 <101282838+zzzt4@users.noreply.github.com>
2023-11-25 13:40:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function function1() {
+  
+    document.getElementById("aud1").play();
+  }
+  
+  function function2() {
+    
+    document.getElementById("aud2").play();
+  }
+  
+  function function3() {
+    
+    document.getElementById("aud3").play();
+  }
+  
+  function function4() {
+    
+    document.getElementById("aud4").play();
+  }
+  
+  function function5() {
+    
+    document.getElementById("aud5").play();
+  }
+  
+  function function6() {
+    
+    document.getElementById("aud6").play();
+  }
